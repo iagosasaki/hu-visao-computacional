921a0a1ae0e41c5aaa7216142c6d38e013d47353
iagosasaki <Sasaki_VG@hotmail.com>
2025-11-11 21:02:04 -0300

    feat:username input and application workflow

--- main.py
+++ main.py
@@ -2,50 +2,102 @@ import numpy as np
 import cv2 
 import time
 from pynput.keyboard import Controller
+import tkinter as tk
+from tkinter import messagebox
 
+inputFrameName = None
 keyboard = Controller()
+studentName = None
 
-width, height = 1280, 720
-cam = cv2.VideoCapture(0)
-cam.set(3, width)
-cam.set(4, height)
+def main():
+    initScreen()
 
-start_time = 0
-elapsed_time = 0
-running = False
+def stopSimulator():
+    global inputFrameName
+    if messagebox.askyesno("Fechar Simulador", "Tem certeza que deseja fechar o simulador?"):
+        if inputFrameName is not None:
+            inputFrameName.destroy()
+        cv2.destroyAllWindows()
+        exit()
 
-while True:
-    success, img = cam.read()
-    if not success:
-        print("Erro ao acessar camera.")
-        break
+def storeStudentName():
+    global studentName,inputFrameName
 
-    if running:
-        elapsed_time = time.time() - start_time
+    studentName = studentName.get().strip()
+    if studentName == "":
+        messagebox.showwarning("Aviso", "Por favor, insira o nome do aluno.")
+    else:
+        messagebox.showinfo("Bem-vindo", f"Bem-vindo ao Simulador de Cirurgia, {studentName}!")
+        inputFrameName.destroy()
+        initCam()
 
-    minutos = int(elapsed_time // 60)
-    segundos = int(elapsed_time % 60)
-    timer = f"{minutos:02}:{segundos:02}"
+def initScreen():
+    global inputFrameName, studentName
 
-    cv2.putText(img, f"Tempo: {timer}", (50, 70),
-                cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3, cv2.LINE_AA)
-    cv2.putText(img, "Pressione:'t' para iniciar/zerar e 'c' para sair",
-                (50, height - 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+    studentName = None
 
-    cv2.imshow("Image", img)
-    keyCV = cv2.waitKey(1) & 0xFF
+    inputFrameName = tk.Tk()
+    inputFrameName.title("Simulador de Cirurgia - HU")
+    inputFrameName.geometry("300x200")
 
-    if keyCV == ord('c'):
-        break
+    studentName = tk.StringVar()
 
-    if keyCV == ord('t'):
-        if not running:
-            start_time = time.time()
-            elapsed_time = 0
-            running = True
-        else:
-            running = False
-            elapsed_time = 0
+    tk.Label(inputFrameName, text="Nome do Aluno:").pack(pady=5)
+    tk.Entry(inputFrameName, textvariable=studentName, width=30).pack(pady=5)
+    tk.Button(inputFrameName, text="Iniciar Simulador", command=storeStudentName).pack(pady=10)
+    tk.Button(inputFrameName, text="Fechar Simulador", command=stopSimulator).pack(pady=10)
+    
+    tk.mainloop()
 
-cam.release()
-cv2.destroyAllWindows()
+def initCam():
+    width, height = 1280, 720
+    cam = cv2.VideoCapture(0)
+    cam.set(3, width)
+    cam.set(4, height)
+
+    startTime = 0
+    elapsedTime = 0
+    isRunning = False
+
+    while True:
+        success, img = cam.read()
+        if not success:
+            print("Erro ao acessar camera.")
+            break
+
+        if isRunning:
+            elapsedTime = time.time() - startTime
+
+        minutes = int(elapsedTime // 60)
+        seconds = int(elapsedTime % 60)
+        timer = f"{minutes:02}:{seconds:02}"
+
+        cv2.putText(img, f"Tempo: {timer}", (50, 70),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3, cv2.LINE_AA)
+        cv2.putText(img, f"Aluno: {studentName}", (width - 400, 70),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3, cv2.LINE_AA)
+        cv2.putText(img, "Pressione:'t' para iniciar/zerar e 'c' para sair",
+                    (50, height - 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.imshow("Image", img)
+        keyCV = cv2.waitKey(1) & 0xFF
+
+        if keyCV == ord('c'):
+            if(timer == "00:00"):
+                break
+
+        if keyCV == ord('t'):
+            if not isRunning:
+                startTime = time.time()
+                elapsedTime = 0
+                isRunning = True
+            else:
+                isRunning = False
+                elapsedTime = 0
+
+    cam.release()
+    cv2.destroyAllWindows()
+    initScreen()
+
+if __name__ == "__main__":
+    main()
